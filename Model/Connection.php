d162bc8fb008be4fe2cdac96eb7c9067623b82ad
ReynanSantts <reynanmesquita016@gmail.com>
2025-07-09 08:04:12 -0300

    Introdução a banco de dados com php

---
+++ Model/Connection.php
@@ -0,0 +1,31 @@
+<?php
+
+// Configurações de uso
+
+// Exemplos de uso em outras classes = use Model/
+namespace Model;
+
+// Importação para conexão do banco de dados
+use PDO;
+use PDOException;
+
+// Buscando dados de configuração de Banco de Dados
+require __DIR__ ."../Config/configuration.php";
+
+class Connection {
+    private  static $stmt;
+
+    // Conexão com o banco de dados
+    public static function getInstance(){
+        // Criar uma nova conexao somente se ela não existir
+        try{
+             if(empty((self::$stmt))){
+            self::$stmt = new PDO("mysql:host=" . DB_HOST . ";port=" . DB_PORT.";dbname=".DB_NAME. '',DB_USER, DB_PASSWORD);
+        }
+        } catch(PDOException $error ) {
+            die("Erro ao estabelecer conexão ". $error->getMessage());
+        }
+        return self::$stmt;
+    }
+}
+?>
---
+++ Model/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Model;
+
+use Model\Connection;
+
+use PDO;
+use PDOException;
+
+class User {
+    private $db;
+
+    /**
+     * Metodo que ira ser executado toda vez que
+     * for criado de um objeto de classe -> USER
+     */
+    public function __construct() {
+        $this->db = Connection::getInstance();
+    }
+
+    // Função de criar usuario
+        public function registerUser($user_fullname, $email, $password) {
+            try{
+                // Inserção de dados na linguagem sql
+                $sql = "INSERT INTO user (user_fullname, email, password, created_at) VALUES (:user_fullname, :email, :password , NOW())";
+
+                //Preparar o banco de dados para receber o comando acima
+                $stmt = $this->db->prepare($sql);
+
+                // Referenciar os dados passados pelo comando sql com os parametros da função
+                $stmt->bindParam(":user_fullname", $user_fullname, PDO::PARAM_STR);
+                // Executar tudo
+
+            }catch(PDOException $error){};
+        }
+}
+
+?>
