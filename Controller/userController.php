fb381f3fe34a399ea42d1e84ac14867adbaa7e81
ReynanSantts <reynanmesquita016@gmail.com>
2025-07-17 08:53:21 -0300

    feat: Conclusão userController

--- Controller/userController.php
+++ Controller/userController.php
@@ -5,47 +5,67 @@ namespace Controller;
 use Model\User;
 use Exception;
 
-class UserController{
+class UserController
+{
     private $userModel;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->userModel = new User();
     }
 
     //Registro de Usuario
-    public function registerUser($user_fullname, $email, $password){
-        try{
+    public function registerUser($user_fullname, $email, $password)
+    {
+        try {
 
-            if(empty($user_fullname) or empty($email) or empty ($password)){
+            if (empty($user_fullname) or empty($email) or empty($password)) {
                 return false;
             }
-             $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
             return $this->userModel->registerUser($user_fullname, $email, $hashedPassword);
-        }
-        catch(Exception $e){
+        } catch (Exception $e) {
             echo "Erro ao cadastrar usuario:" . $e->getMessage();
             return false;
         }
     }
+    //E-mail ja cadastrado?
+    public function checkUserByEmail($email){
+        return $this->userModel->getUserByEmail($email);
+    }
+
     //Login de Usuario
-    public function login($email, $password){
+    public function login($email, $password)
+    {
         $user = $this->userModel->getUserByEmail($email);
 
-        if($user){
-            if(crypt($password, $user['password'])){
-                $_SESSION['id'] = $user['id'];
-                $_SESSION['user_fullname'] = $user['user_fullname'];
-                $_SESSION['email'] = $user['email'];
-                return true;
-            }else {
-                return false;
-            }
+        /* $user = [
+            "id" => 1,
+            "user_fullname" => "Teste"
+            "email" => "reynan@example.com"
+            "password" => "......"
+            ]
+        */
+
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['id'] = $user['id'];
+            $_SESSION['user_fullname'] = $user['user_fullname'];
+            $_SESSION['email'] = $user['email'];
         }
+        return false;
     }
 
     //Usuario logado?
+    public function isLoggedIn(){
+        return isset($_SESSION['id']);
+    }
 
     //Resgatar dados do usuario
+    public function getUserData($id,$user_fullname, $email){
+        $id = $_SESSION['id'];
+
+        return $this->userModel->getUserInfo($id,$user_fullname, $email);
+    }
 }
 ?>
